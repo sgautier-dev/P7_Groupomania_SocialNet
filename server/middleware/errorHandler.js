41dad5b46cb231e22a8f50764c54495b7b979ea0
sebdina <95606603+sebdina@users.noreply.github.com>
2022-09-13 16:09:42 +0400

    adding logger and errorHandler middlewares

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,14 @@
+const {logEvents} = require('./logger');
+
+//appending errors to logs
+const errorHandler = (err, req, next) => {
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log');
+    console.log(err.stack);
+
+    const status = res.statusCode ? res.statusCode : 500; //if no status code set to server error
+
+    res.status(status);
+    res.json({message: err.message});
+};
+
+module.exports = errorHandler;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -13,10 +19,12 @@ app.all('*', (req, res) => {
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({message: '404 Not Found'})
+        res.json({ message: '404 Not Found' })
     } else {
         res.type('txt').send('404 Not Found')
     }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
