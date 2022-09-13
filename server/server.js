5b3f53825c2f510d6d3015ce1d252b08d3ff84fa
sebdina <95606603+sebdina@users.noreply.github.com>
2022-09-13 15:00:57 +0400

    404 page and route added

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
+
+// setting routes to 404 if no other route is matching
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+});
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
