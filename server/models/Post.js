7fe2b47eacce6d45235a9bb7bbd9a42c46a52f4b
sgautier-dev <sgautier.dev@gmail.com>
2022-09-14 11:04:13 +0400

    models and mongodb connection set

---
+++ server/models/Post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        likes: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
+                },
+            },
+        ],
+        // date: {
+        //     type: Date,
+        //     default: Date.now,
+        // },
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Post', postSchema);
