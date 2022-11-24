07c41765a644c87174e23aa7e065e7b9d99918b4
sgautier-dev <sgautier.dev@gmail.com>
2022-11-24 10:38:31 +0400

    ready to deploy

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://grouponet.onrender.com',
     //to attach cookie in requests
     credentials: 'include',
     //setting authorization header with current token
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,6 +3,8 @@ import { apiSlice } from './api/apiSlice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import authReducer from '../features/auth/authSlice';
 
+const devMode = process.env.NODE_ENV === 'development' ? true : false;
+
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
@@ -11,7 +13,7 @@ export const store = configureStore({
     //default middleware required with RTK query
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
+    devTools: devMode
 });
 
 setupListeners(store.dispatch); //to access to options like being able to refetch data on intervall in query hooks
