f14246aab71970d20a5813910386849668627ba0
sgautier-dev <sgautier.dev@gmail.com>
2022-12-02 10:03:40 +0400

    test for safari cookie issue

--- server/middleware/cookies.js
+++ server/middleware/cookies.js
@@ -1,9 +1,9 @@
 
 const clearCookieOptions = {
     httpOnly: true, //accessible only by web server 
-    domain: process.env.NODE_ENV === 'development' ? 'localhost' : 'grouponet.onrender.com',
+    domain: process.env.NODE_ENV === 'development' ? 'localhost' : process.env.COOKIE_PROD_DOMAIN,
     secure: process.env.NODE_ENV === 'production' ? true : false, //https
-    sameSite: 'Strict', //cross-site cookie 
+    sameSite: process.env.COOKIE_SAMESITE, //cross-site cookie 
 }
 
 console.log('clearCookieOptions', clearCookieOptions)
